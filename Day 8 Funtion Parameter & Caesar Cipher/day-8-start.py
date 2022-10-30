a3b9b6a504d7b086aa962e4a8a30660986d7bbbf
tonioqiu <tonioqiu.dev@gmail.com>
2022-10-30 16:07:37 -0500

    added day 8

---
+++ Day 8 Funtion Parameter & Caesar Cipher/day-8-start.py
@@ -0,0 +1,17 @@
+def greet():
+    print("Hellow")
+    print("Good Morning")
+    print("How are you")
+
+
+greet()
+
+#function with input
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}?")
+
+#calling function
+def greet_with(name,location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
